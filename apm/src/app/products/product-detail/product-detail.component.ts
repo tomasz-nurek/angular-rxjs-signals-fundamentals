c10113cfa0ffc1929ee11a56667dafada6a3fbba
U722566 <tomasz.nurek@lhsystems.com>
2024-03-18 15:44:37 +0100

    - added httpClient to app.config.ts - implemented product.service.ts getProducts and getProduct($id) - implemented display of product details

--- apm/src/app/products/product-detail/product-detail.component.ts
+++ apm/src/app/products/product-detail/product-detail.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input } from '@angular/core';
+import {Component, inject, Input, OnChanges, OnDestroy, SimpleChanges} from '@angular/core';
 
-import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
-import { Product } from '../product';
+import {NgIf, NgFor, CurrencyPipe} from '@angular/common';
+import {Product} from '../product';
+import {ProductService} from "../product.service";
+import {Subscription, tap} from "rxjs";
 
 @Component({
-    selector: 'pm-product-detail',
-    templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, CurrencyPipe]
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges, OnDestroy {
   // Just enough here for the template to compile
   @Input() productId: number = 0;
   errorMessage = '';
@@ -19,6 +21,24 @@ export class ProductDetailComponent {
 
   // Set the page title
   pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  private productService: ProductService = inject(ProductService);
+  private productSub!: Subscription;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const id = changes['productId'].currentValue;
+    if (id) {
+      this.productId = id;
+      this.productSub = this.productService.getProductById(id)
+        .pipe(tap(() => console.log("In tap product detail sub pipeline")))
+        .subscribe(product => this.product = product);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.productSub) {
+      this.productSub.unsubscribe();
+    }
+  }
 
   addToCart(product: Product) {
   }
